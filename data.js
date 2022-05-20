8c85433a33a059637338d22325a32f4b2573b611
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-05-20 13:24:59 -0300

    Improve data

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-
 const DATA = {
   "acordion.jpeg": [
     "Acordion",
@@ -11,31 +10,31 @@ const DATA = {
   ],
   "burger1.jpeg": [
     "Burger",
-    'A hamburger is a food consisting of fillings —usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll. Hamburgers are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chilis; condiments such as ketchup, mustard, mayonnaise, relish, or a "special sauce", often a variation of Thousand Island dressing; and are frequently placed on sesame seed buns.',
+    "A hamburger is a food consisting of fillings —usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll.",
   ],
   "burger2.jpeg": [
     "Gourmet Burger",
-    'A hamburger is a food consisting of fillings —usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll. Hamburgers are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chilis; condiments such as ketchup, mustard, mayonnaise, relish, or a "special sauce", often a variation of Thousand Island dressing; and are frequently placed on sesame seed buns.',
+    'Hamburgers are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chilis; condiments such as ketchup, mustard, mayonnaise, relish, or a "special sauce", often a variation of Thousand Island dressing; and are frequently placed on sesame seed buns.',
   ],
   "burger3.jpeg": [
     "Cheedar Burger",
-    'A hamburger is a food consisting of fillings —usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll. Hamburgers are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chilis; condiments such as ketchup, mustard, mayonnaise, relish, or a "special sauce", often a variation of Thousand Island dressing; and are frequently placed on sesame seed buns.',
+    'A hamburger is a food consisting of fillings —usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll. Condiments such as ketchup, mustard, mayonnaise, relish, or a "special sauce", often a variation of Thousand Island dressing; and are frequently placed on sesame seed buns.',
   ],
   "cake1.jpeg": [
     "Dark Cake",
-    "Cake is a form of sweet food made from flour, sugar, and other ingredients, that is usually baked. In their oldest forms, cakes were modifications of bread, but cakes now cover a wide range of preparations that can be simple or elaborate, and that share features with other desserts such as pastries, meringues, custards, and pies.",
+    "Cake is a form of sweet food made from flour, sugar, and other ingredients, that is usually baked.",
   ],
   "cake2.jpeg": [
     "White Cake",
-    "Cake is a form of sweet food made from flour, sugar, and other ingredients, that is usually baked. In their oldest forms, cakes were modifications of bread, but cakes now cover a wide range of preparations that can be simple or elaborate, and that share features with other desserts such as pastries, meringues, custards, pies and.",
+    "In their oldest forms, cakes were modifications of bread, but cakes now cover a wide range of preparations that can be simple or elaborate, and that share features with other desserts such as pastries, meringues, custards, pies and.",
   ],
   "cake3.jpeg": [
     "Rainbow Cake",
-    "Cake is a form of sweet food made from flour, sugar, and other ingredients, that is usually baked. In their oldest forms, cakes were modifications of bread, but cakes now cover a wide range of preparations that can be simple or elaborate, and that share features with other desserts such as pastries, meringues, custards, and pies.",
+    "Cakes were modifications of bread, but cakes now cover a wide range of preparations that can be simple or elaborate, and that share features with other desserts such as pastries, meringues, custards, and pies.",
   ],
   "cleaning.jpeg": [
     "Cleaning Kit",
-    "lean Up To 10 Different Surfaces With Our Bleach Free Cleaner Spray! All New Clorox® Multi-Purpose Refillable Cleaner Starter Kit With Crisp Lemon Fragrance",
+    "lean Up To 10 Different Surfaces With Our Bleach Free Cleaner Spray! All New Clorox Multi-Purpose Refillable Cleaner Starter Kit With Crisp Lemon Fragrance",
   ],
   "cleaning2.jpeg": [
     "Cleaning Gloves",
@@ -59,27 +58,27 @@ const DATA = {
   ],
   "icecream.jpg": [
     "Ice Cream",
-    "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. It may be made from milk or cream and is flavoured with a sweetener, either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
+    "It may be made from milk or cream and is flavoured with a sweetener, either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
   ],
   "icecream0.jpeg": [
     "Water Melon Ice Cream",
-    "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. It may be made from milk or cream and is flavoured with a sweetener, either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
+    "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. Spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
   ],
   "icecream1.jpeg": [
     "Ice Cream Bouquet",
-    "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. It may be made from milk or cream and is flavoured with a sweetener, either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
+    "Ice cream may be made from milk or cream and is flavoured with a sweetener, either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
   ],
   "icecream2.jpeg": [
     "Granizado",
-    "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. It may be made from milk or cream and is flavoured with a sweetener, either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
+    "Frozen food typically eaten as a snack or dessert. It may be made from milk or cream and is flavoured with a sweetener.",
   ],
   "icecream3.jpeg": [
     "Purple Ice Cream",
-    "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. It may be made from milk or cream and is flavoured with a sweetener, either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
+    "Made from milk or cream and is flavoured with a sweetener, either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches.",
   ],
   "milanesa.jpg": [
     "Milanesa",
-    "The milanesa is a South American variation of the Lombard Veal Milanese, or the Austrian Wiener schnitzel, where generic types of meat breaded cutlet preparations are known as a milanesa.",
+    "The milanesa is a South American variation of the Lombard Veal Milanese, or the Austrian Wiener schnitzel.",
   ],
   "phone1.jpeg": [
     "Office Phone",
@@ -91,35 +90,32 @@ const DATA = {
   ],
   "pizza.jpeg": [
     "Pizza Morron",
-    "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.",
+    "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients.",
   ],
-  "pizza.jpg": [
-    "Pizza",
-    "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.",
-  ],
+  "pizza.jpg": ["Pizza", "A small pizza is sometimes called a pizzetta."],
   "pizza1.jpeg": [
     "Pizza Veggie",
-    "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.",
+    "Flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.",
   ],
   "pizza2.jpeg": [
     "Pizza Gourmet",
-    " Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.",
+    "Tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.",
   ],
   "plant1.jpeg": [
     "Shadow Plant",
-    "plant, (kingdom Plantae), any multicellular eukaryotic life-form characterized by (1) photosynthetic nutrition (a characteristic possessed by all plants except some parasitic plants and underground orchids), in which chemical energy is produced from water, minerals, and carbon dioxide with the aid of pigments and the radiant energy of the Sun, (2) essentially unlimited growth at localized regions, (3) cells that contain cellulose in their walls and are therefore to some extent rigid,",
+    "Photosynthetic nutrition (a characteristic possessed by all plants except some parasitic plants and underground orchids), in which chemical energy is produced from water, minerals, and carbon dioxide with the aid of pigments and the radiant energy of the Sun, (2) essentially unlimited growth at localized regions, (3) cells that contain cellulose in their walls and are therefore to some extent rigid,",
   ],
   "plant2.jpeg": [
     "Desert Plant",
-    "plant, (kingdom Plantae), any multicellular eukaryotic life-form characterized by (1) photosynthetic nutrition (a characteristic possessed by all plants except some parasitic plants and underground orchids), in which chemical energy is produced from water, minerals, and carbon dioxide with the aid of pigments and the radiant energy of the Sun, (2) essentially unlimited growth at localized regions, (3) cells that contain cellulose in their walls and are therefore to some extent rigid,",
+    "plant, (kingdom Plantae), any multicellular eukaryotic life-form characterized by (1) photosynthetic nutrition",
   ],
   "plant3.jpeg": [
     "Shadow Plant 2 ",
-    "plant, (kingdom Plantae), any multicellular eukaryotic life-form characterized by (1) photosynthetic nutrition (a characteristic possessed by all plants except some parasitic plants and underground orchids), in which chemical energy is produced from water, minerals, and carbon dioxide with the aid of pigments and the radiant energy of the Sun, (2) essentially unlimited growth at localized regions, (3) cells that contain cellulose in their walls and are therefore to some extent rigid,",
+    "multicellular eukaryotic life-form characterized by (1) photosynthetic nutrition (a characteristic possessed by all plants aid of pigments and the radiant energy of the Sun, (2) essentially unlimited growth at localized regions, (3) cells that contain cellulose in their walls and are therefore to some extent rigid,",
   ],
   "violin.jpeg": [
     "Violin",
-    "The violin, sometimes known as a fiddle, is a wooden chordophone in the violin family. Most violins have a hollow wooden body. It is the smallest and thus highest-pitched instrument in the family in regular use",
+    "The violin, sometimes known as a fiddle, is a wooden chordophone in the violin family. Most violins have a hollow wooden body.",
   ],
 };
 
